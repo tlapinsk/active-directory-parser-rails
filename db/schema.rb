dee88dee0249ee685b85911119955e188f5cfe10
Tim Lapinskas <tim.lapinskas@gmail.com>
2017-03-02 16:29:04 -0800

    Created Users table

--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,12 @@ ActiveRecord::Schema.define(version: 20170302233137) do
     t.string   "name"
     t.string   "job"
     t.string   "email"
-    t.integer  "shoretel"
-    t.integer  "cell"
-    t.integer  "fax"
+    t.integer  "shoretel",   limit: 8
+    t.integer  "cell",       limit: 8
+    t.integer  "fax",        limit: 8
     t.string   "group"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
   end
 
 end
