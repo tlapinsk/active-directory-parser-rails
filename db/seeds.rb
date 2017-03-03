cee0bbdcffb9a09648d977320a6d7371cd7a8049
Tim Lapinskas <tim.lapinskas@gmail.com>
2017-03-03 09:47:58 -0800

    Completed seeding the User table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'UsersMembers.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+	t = User.new
+	t.name = row['Name']
+	t.job = row['Job']
+	t.email = row['Email']
+	t.shoretel = row['ShoreTel']
+	t.cell = row['Cell']
+	t.fax = row['Fax']
+	t.group = row['MemberOf']
+	t.save
+	puts "#{t.name} saved"
+end
+
+puts "There are now #{User.count} rows in the User table"
